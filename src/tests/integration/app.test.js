ebf5a98b37290cb42f1d4ff5cab9805ea3feca01
Og J. Ramos <og.ramos@staffbase.com>
2022-08-23 21:59:40 -0600

    Updated some files for initial commit.

--- src/tests/integration/app.test.js
+++ src/tests/integration/app.test.js
@@ -1,9 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
+let __importDefault = (this && window.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../../src/app"));
+const app_1 = __importDefault(require("../../app"));
 const supertest_1 = __importDefault(require("supertest"));
 describe("GET / - a simple api endpoint", () => {
     it("should return 200 OK", () => {
